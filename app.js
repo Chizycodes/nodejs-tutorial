0101f784a618f9f54be14f20dd81132e5dac0dee
Chizycodes <okwudirejoy@gmail.com>
2022-12-25 16:11:18 +0100

    add nodemon

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const http = require("node:http");
 
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(
+			JSON.stringify({
+				data: 'Hello World!',
+			})
+		);
+})
 
-console.log('Chizycodes')
+console.log(`server is running on port 3000`)
+server.listen(3000);
