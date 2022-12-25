46901900356472aab8ed7b9db2517894c05dcea5
Chizycodes <okwudirejoy@gmail.com>
2022-12-25 18:17:58 +0100

    express routing

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
-const EventEmitter = require('events');
-const myEvent = new EventEmitter();
+// const EventEmitter = require('events');
+// const myEvent = new EventEmitter();
 
-myEvent.on('test-event', (data)=>{
-    console.log('test event is fired');
-    console.log(data);
-})
+// myEvent.on('test-event', (data)=>{
+//     console.log('test event is fired');
+//     console.log(data);
+// })
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 5000;
+const routes = require('./routes/index');
 
-app.get('/', (req, res) => {
-    myEvent.emit('test-event', {name: 'john', age: 30});
-    res.send('Hello World');
-})
+app.use(routes);
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`);
-})
+app.listen(PORT, () => {
+	console.log(`server is running on port ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const bookRouter = require('./book');
+
+router.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+router.use('/book',bookRouter);
+
+
+router.all('/*', (req, res) => {
+	res.send('404 not found');
+});
+
+module.exports = router;
