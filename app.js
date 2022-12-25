ccbf1e086420c82aad6bb25043871c387ca7ae03
Chizycodes <okwudirejoy@gmail.com>
2022-12-25 16:39:25 +0100

    learn to read files

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const http = require('node:http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 	console.log(req.url);
 	if (req.url === '/') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write('<h1>Home Page</h1>');
-		res.end();
-	}
-	if (req.url === '/about') {
-		res.writeHead(200, { 'Content-Type': 'text/html' });
-		res.write('<h1>About Page</h1>');
-		res.end();
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.readFile('pages/home.html', 'utf8', (err, data) => {
+			if (err) throw err;
+			res.write(data);
+			res.end();
+		});
+	} else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+		fs.readFile('pages/about.html', 'utf8', (err, data) => {
+			if (err) throw err;
+			res.write(data);
+			res.end();
+		});
 	} else {
-		res.writeHead(404, { 'Content-Type': 'text/html' });
-		res.write('<h1>404 Page Not Found</h1>');
-		res.end();
+        res.writeHead(404, {'Content-Type': 'text/html'});
+		fs.readFile('pages/404.html', 'utf8', (err, data) => {
+			if (err) throw err;
+			res.write(data);
+			res.end();
+		});
 	}
 });
 
