c10728ac05567fcf29e25105f6d288f92d488613
Chizycodes <okwudirejoy@gmail.com>
2022-12-26 15:36:44 +0100

    express validation with mongoosw

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -1,36 +1,34 @@
-const connect = require('../database/db');
 const { ObjectId } = require('mongodb');
+const Book = require('../models/Book');
 
 exports.index = async (req, res) => {
-	const db = await connect();
-	const books = await db.collection('books').find().toArray();
+	const books = await Book.find();
 	res.json(books);
 };
 
 exports.store = async (req, res) => {
-	const db = await connect();
-
-	await db.collection('books').insertOne(req.body);
-	res.status(201).json({ data: 'Book saved successfully' });
+	try {
+		await Book.create(req.body);
+		res.status(201).json({ data: 'Book saved successfully' });
+	} catch (error) {
+		res.status(400).json(error.errors);
+	}
 };
 
 exports.show = async (req, res) => {
 	const _id = ObjectId(req.params.id);
-	const db = await connect();
-	const book = await db.collection('books').find({ _id }).toArray();
+	const book = await Book.find({ _id });
 	res.json(book);
 };
 
 exports.update = async (req, res) => {
 	const _id = ObjectId(req.params.id);
-	const db = await connect();
-	await db.collection('books').updateOne({ _id }, { $set: req.body });
+	await Book.updateOne({ _id }, { $set: req.body });
 	res.json({ data: 'Book updated successfully' });
 };
 
 exports.delete = async (req, res) => {
 	const _id = ObjectId(req.params.id);
-	const db = await connect();
-	await db.collection('books').deleteOne({ _id });
+	await Book.deleteOne({ _id });
 	res.status(204).json();
 };
--- main.js
+++ main.js
@@ -1,21 +1,16 @@
-// const EventEmitter = require('events');
-// const myEvent = new EventEmitter();
-
-// myEvent.on('test-event', (data)=>{
-//     console.log('test event is fired');
-//     console.log(data);
-// })
-
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = 5000;
 const routes = require('./routes/index');
+const connectDB = require('./database/db');
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(routes);
 
-app.listen(PORT, () => {
-	console.log(`server is running on port ${PORT}`);
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log(`server is running on port ${PORT}`);
+	});
 });
